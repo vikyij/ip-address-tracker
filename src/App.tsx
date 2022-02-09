59807797e507c7d890bcc0ed978b452773b09179
vikyij <vikkyij54@gmail.com>
2022-02-09 21:04:25 +0100

    integrate api to retrieve location and display on map

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
+import { getLocation } from './components/api'
 import Header from './components/Header'
 import Map from './components/map'
 
-function App() {
+interface Location {
+  ip: string
+  isp: string
+  location: {
+    country: string
+    region: string
+    timezone: string
+    lat: number
+    lng: number
+  }
+}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState<string>('')
+  const [data, setData] = useState<Location | undefined>()
+
+  const getIPInformation = () => {
+    getLocation(searchTerm)
+      .then((data) => {
+        setData(data?.data)
+      })
+      .catch((err) => console.log('error', err))
+  }
+
+  useEffect(() => {
+    getIPInformation()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleSearch = (input: string) => {
+    setSearchTerm(input)
+  }
   return (
     <>
-      <Header />
-      <Map />
+      <Header
+        ip={data?.ip}
+        isp={data?.isp}
+        country={data?.location?.country}
+        region={data?.location?.region}
+        timezone={data?.location?.timezone}
+        handleSearch={handleSearch}
+        getIPInformation={getIPInformation}
+      />
+      {data && <Map lat={data?.location?.lat} lng={data?.location?.lng} />}
     </>
   )
 }
 
-export default App;
+export default App
--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,27 +1,37 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import L from 'leaflet'
+import MarkerIcon from '../../assets/images/icon-location.svg'
 
-const Map = () => {
-return (
-  <div>
-  <MapContainer
-    center={[51.505, -0.09]}
-    zoom={13}
-    scrollWheelZoom={false}
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  >
-    <TileLayer
-      //attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
-</div>
-)
+const Icon = new L.Icon({
+  iconUrl: MarkerIcon,
+  iconRetinaUrl: MarkerIcon,
+  popupAnchor: [-0, -0],
+  iconSize: [32, 45],
+})
+
+interface HeaderProps {
+  lat: number | undefined
+  lng: number | undefined
+}
+
+const Map: React.FC<HeaderProps> = ({ lat, lng }) => {
+  let userPosition: [number, number] = [lat ?? 0, lng ?? 0]
+  return (
+    <div>
+      <MapContainer
+        center={userPosition}
+        zoom={15}
+        scrollWheelZoom={false}
+        doubleClickZoom={true}
+        zoomControl={false}
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      >
+        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+        <Marker position={userPosition} icon={Icon}></Marker>
+      </MapContainer>
+    </div>
+  )
 }
 
 export default Map
